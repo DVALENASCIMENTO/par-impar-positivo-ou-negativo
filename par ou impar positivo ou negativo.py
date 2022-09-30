8481a2e77ae7758cec29a72d4f4533344c8943cf
DIEGO VALE DO NASCIMENTO <105137007+DVALENASCIMENTO@users.noreply.github.com>
2022-09-30 01:44:45 -0300

    Python par-impar-positivo-ou-negativo
    Programa em Python para classificar o número informado pelo usuário se é par, ímpar, positivo ou negativo,

---
+++ par ou impar positivo ou negativo.py
@@ -0,0 +1,15 @@
+# Descrição: Informar os números em ordem crescente
+# Autor: Diego Vale do Nascimento - 06/09/2022
+
+n1 = int(input("Informe um número inteiro: "))
+
+if (n1 > 0):
+    print("O número informado é positivo")
+else:
+    print("o número informado é negativo")
+
+if (n1%2 == 0):
+    print("O número informado é par")
+else:
+    print("o número informado é ímpar")
+
